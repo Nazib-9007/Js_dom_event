9252669e960b66e5bcd1355ac7e5aa73bf25bd83
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-03-17 00:16:18 +0600

    Refactor JavaScript functions and DOM manipulations

--- index.js
+++ index.js
@@ -1,3 +1,42 @@
+//JS Function Tutorial...
+//using normal function...
+function findVowel(str) {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+//using arrow function...
+const searchVowel = (str) => {
+    let count = 0;
+    for (let i = 0; i < str.length; i++) {
+        let char = str[i].toLowerCase();
+        if (char === "a" || char === "e" || char === "i" || char === "o" || char === "u") {
+            count++;
+        }
+    }
+    return count;
+}
+//For each loop. it act's like a method...
+let arr = [1,2,3,4,5];
+arr.forEach(function printValue(val){
+    console.log(val);
+})
+//For a given array of numbers, print the squeare of each value using foreach loop
+let arr = [2,3,6,8,10];
+arr.forEach((arr)=>{
+    console.log(arr);
+})
+arr.forEach((arr)=>{
+    console.log(arr**2);
+});
+
+//Function tutorial end...
+
 //Js inserAdjacentHTML() methods...
 // It has four values which is--> 'Beforebeing', 'afterbeing', 'beforend', 'afterend'....
 
