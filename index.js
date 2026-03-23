14d23ec0dc123ae7fd9434db74547980a945ac5d
_@Nazib_07 <nazib9007.ice.bup@gmail.com>
2026-03-24 00:06:10 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,37 +4,36 @@ let button = document.querySelector('#btn');
 
 
 /// at first remove the comment...
-// button.addEventListener('click', listen);
-// form.addEventListener('click', listen);
-// parent.addEventListener('click', listen);
+button.addEventListener('click', listen);
+form.addEventListener('click', listen);
+parent.addEventListener('click', listen);
 
-//Js Bubbling...
-//Here atfirst come child element then parent element..
-// function listen (enent) {
-//     console.log(event.currentTarget);
-//}
+Js Bubbling...
+Here atfirst come child element then parent element..
+function listen (enent) {
+    console.log(event.currentTarget);
+}
 
-// function listen (event){
-//     console.log(this.tagName);
-// }
+function listen (event){
+    console.log(this.tagName);
+}
 
 //Js Capturing...
-//It just reverse the tag name. parent parent come first then child...
+It just reverse the tag name. parent parent come first then child...
 
-// button.addEventListener('click', listen, {
-//     capture: true,
-// });
-// form.addEventListener('click', listen, {
-//     capture: true,
-// });
-// parent.addEventListener('click', listen, {
-//     capture: true,
-// });
+button.addEventListener('click', listen, {
+    capture: true,
+});
+form.addEventListener('click', listen, {
+    capture: true,
+});
+parent.addEventListener('click', listen, {
+    capture: true,
+});
 
-// function listen (){
-//     console.log(this.tagName);
-// }
-
+function listen (){
+    console.log(this.tagName);
+}
 
 let button = document.querySelector('#btn');
 button.addEventListener('click', (e)=>{
@@ -46,3 +45,15 @@ button.addEventListener('click', (e)=>{
     let mess = document.querySelector('#message');
     mess.innerHTML = `Keys are ${key.join('+')}`;
 });
+
+
+//movemove event
+
+let bgColor = document.querySelector('#bg');
+bgColor.addEventListener('mousemove', (e)=>{
+    let mess = document.querySelector('#mess');
+    mess.innerHTML = `
+    Screen is ${e.screenX}, ${e.screenY}`
+    let messa = document.querySelector('#messa');
+    messa.innerHTML = `Client is ${e.clientX}, ${e.clientY}`;
+})
